abdfe6812564e12a509d5eff696e44c314dbdd0a
AndaLatif <andaramona.latif@gmail.com>
2022-10-03 22:40:32 +0200

    videos added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.js',
+  entry:'./src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: "bundle.js",
   },
-};
+}
