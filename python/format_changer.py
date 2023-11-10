c1a7f75dabe7a11accda623acc0bd62a7aec56c0
Philipp Schlicht <european.set.theory@gmail.com>
2023-11-10 12:05:49 +0100

    update

--- python/format_changer.py
+++ python/format_changer.py
@@ -6,5 +6,8 @@ files_to_convert=["data", "records", "blog"]
 for file_of_data in files_to_convert:
     file_name=file_of_data+".xlsx"
     df_to_convert = pd.read_excel(file_name)
+    if file_of_data=="records":
+        df_to_convert=df_to_convert.sort_values(axis=0,by=['Speaker'])
     new_file="../public/"+file_of_data+".csv"
     df_to_convert.to_csv(new_file, index = False, sep=';', quoting=1)
+    
